00e537514fbbf2192f81e847d318aa8116731b48
sselbe <shah.selbe@gmail.com>
2015-07-06 15:46:25 +0200

    Adding archive for Xigera

---
+++ Xigera Lodge Archive/OkaSensor_Data-XBee_pHDO/OkaSensor_Data-XBee_pHDO.ino
@@ -0,0 +1,212 @@
+#include <XBee.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <RTClib.h>
+#include <Adafruit_Sensor.h>
+#include <SoftwareSerial.h>
+
+#define PH_RXPIN         4 // set the pH sensor RX pin (labeled "TX" on pH board)
+#define PH_TXPIN         5 // set the pH sensor TX pin (labeled "RX" on pH board)
+#define D_O_RXPIN        6 // set the dissolved oxygen RX pin (labeled "TX" on DO board)
+#define D_O_TXPIN        7 // set the dissolved oxygen TX pin (labeled "RX" on DO board)
+#define SD_BOARD_PIN     10
+
+float phValue;
+float doValue;
+
+SoftwareSerial pH(PH_RXPIN, PH_TXPIN);
+SoftwareSerial d_o(D_O_RXPIN, D_O_TXPIN); 
+SoftwareSerial xbeeSerial(2, 3); 
+RTC_DS1307 RTC;
+File logfile;
+XBee xbee = XBee();
+
+#pragma pack(push, 1)
+
+typedef struct {
+  float v1;
+  float v2;
+  float v3;
+  float v4;
+  unsigned long time;
+  char kind;
+} packet_t;
+
+packet_t payload;
+
+#pragma pack(push, 4)
+
+// SH + SL Address of receiving XBee
+XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x409F2937); //this needs to be updated for the correct XBee Test configuration: 13A200  40C6746A
+ZBTxRequest zbTx = ZBTxRequest(addr64, (uint8_t *)&payload, sizeof(payload));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  while(1);
+}
+
+void openLogFile()
+{
+  pinMode(SD_BOARD_PIN, OUTPUT);
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SD_BOARD_PIN)) {
+    error("Card failed, or not present");
+  }
+  
+  // create a new file
+  for (long i = 0; i <= 99999999; i++) {
+    char filename[13];
+    String fn(i);
+    while (fn.length() < 8) {
+      fn = '0' + fn;
+    }
+    fn = fn + ".CSV";
+    fn.toCharArray(filename, sizeof(filename));
+    if (!SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      Serial.print("Logging to: ");
+      Serial.println(filename);
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;
+    }
+  }
+
+  if (!logfile) {
+    error("couldnt create file");
+  }
+  logfile.flush();
+}
+
+void setup(){             
+    xbeeSerial.begin(9600);
+    Serial.begin(115200);      
+    xbee.setSerial(xbeeSerial);
+    
+    Wire.begin();
+    RTC.begin();
+    
+    openLogFile();
+}
+
+void loopPh()
+{
+   String rawSensor = "";               
+   boolean gotEnd = false; 
+
+   // same code to run conductivity above except pH only outputs 1 number vs 3 for conductivity sensor
+   pH.begin(9600);           // baud rate for pH sensor = 9600 (known)
+   delay(1000);
+   pH.print("r \r");          // send string "r" with a carriage return "\r" to take one reading
+   delay(3000);               // it takes a while for the pH sensor to respond
+
+   while (pH.available() > 0) {                     // message is being sent              
+      char c = (char)pH.read();             // string msg is read as individual characters       
+      rawSensor += c;                              
+      if (c == '\r') {
+        gotEnd = true;
+       }
+      delay(50);
+   }         
+   pH.end();
+   
+   if (gotEnd){
+      int n = rawSensor.indexOf('*');
+      String phString = rawSensor.substring(0, n);
+      phValue = phString.toFloat();
+   }
+   else{
+      phValue = 9999;
+   }
+}
+
+void loopDissolvedOxygen()
+{
+   String rawSensor = "";               
+   boolean gotEnd = false; 
+
+   d_o.begin(9600);
+   delay(1000);
+   d_o.print("r \r");          // send string "r" with a carriage return "\r" to take one reading
+   delay(3000);                // it takes a while for the pH sensor to respond
+
+   while (d_o.available() > 0) {     
+      char c = (char)d_o.read();                 
+      rawSensor += c;                              
+      if (c == '\r') {
+        gotEnd = true;
+       }
+      delay(50);
+   }         
+   d_o.end();
+   
+   if (gotEnd){
+      int n = rawSensor.indexOf('*');
+      String doString = rawSensor.substring(0, n);
+      doValue = doString.toFloat();
+   }
+   else{
+      doValue = 9999;
+   } 
+}
+
+void loop(){                   
+  Serial.println("Starting new loop...");
+
+  logfile.println("Starting new loop...");
+  logfile.flush();
+
+  DateTime now = RTC.now();
+  loopPh();
+  loopDissolvedOxygen();
+
+  /////////////////////////////////////////////////////////////////////
+  //  Send all data to serial output 
+  /////////////////////////////////////////////////////////////////////
+  logfile.print(now.unixtime());
+  logfile.print(",");
+  logfile.print(phValue);
+  logfile.print(",");
+  logfile.print(doValue);
+  logfile.print(",");
+  logfile.print("\n");
+
+  Serial.print(now.unixtime());
+  Serial.print(",");
+  Serial.print(phValue);
+  Serial.print(",");
+  Serial.print(doValue);
+  Serial.print(",");
+  Serial.print("\n");
+  
+  logfile.flush();
+
+  memset((void *)&payload, 0, sizeof(payload));
+  payload.kind = 0;
+  payload.v1 = phValue;
+  payload.v2 = doValue;
+  payload.time = now.unixtime();
+  xbee.send(zbTx);
+
+  /* Delay for 5 minutes. Watch out for millis() wrapping around and just call that 
+  the end of the delay. This doesn't always need to be 5mins exactly. */
+  unsigned long startMillis = millis();
+  unsigned long lastUpdate = startMillis;
+  unsigned long i = 0;
+  while (true) {
+    unsigned long now = millis();
+    unsigned long elapsed = now - startMillis;
+    if (now - lastUpdate >= 30 * 1000) {
+      Serial.println(elapsed);
+      lastUpdate = now;
+      if (++i == 2 * 5) {
+        break;
+      }
+    }
+    delay(5000);
+  }
+  Serial.println("Done");
+}
---
+++ Xigera Lodge Archive/read-xbee-data.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+import serial
+import struct
+import sys
+import time
+import logging
+import daemon
+import syslog
+import glob
+
+from xbee import ZigBee
+from okavango import SampleUploader
+
+logging.basicConfig(level=logging.DEBUG)
+syslog.openlog(logoption=syslog.LOG_PID)
+
+def phDO(raw):
+    return {
+      'ph' : raw[0],
+      'do' : raw[1]
+    }
+
+def airAlt(raw):
+    return {
+      'humidity' : raw[0],
+      'airTemp' : raw[1],
+      'pressure' : raw[2]
+    }
+
+def airWat(raw):
+    return {
+      'waterTemp' : raw[0],
+      'humidity' : raw[1],
+      'airTemp' : raw[2]
+    }
+
+def condOrp(raw):
+    return {
+      'tds' : raw[0],
+      'salinity' : raw[1],
+      'orp' : raw[2]
+    }
+
+def pressure(raw):
+    return {
+      'humidity' : raw[0],
+      'airTemp' : raw[1],
+      'pressure' : raw[2]
+    }
+
+deserializers = {
+  0: phDO,
+  1: condOrp,
+  2: airWat,
+  3: airAlt,
+  4: pressure
+}
+
+def log(*objects):
+  print(*objects)
+  for i in objects:
+    syslog.syslog(str(i))
+
+def read_a_frame():
+  pass
+
+class XbeeListener(daemon.Daemon):
+  def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+    daemon.Daemon.__init__(self, pidfile, stdin, stdout, stderr)
+
+  def run(self):
+    log("Daemon running...")
+    while True:
+      devices = glob.glob("/dev/ttyUSB*")
+      log("Devices", devices)
+      if len(devices) == 0:
+        time.sleep(1)
+      for deviceName in devices:
+        try:
+          device = serial.Serial(deviceName, 9600, timeout=5, writeTimeout=5)
+          xbee = ZigBee(device)
+
+          while True:
+            try:
+              log("Reading frame...")
+              response = xbee.wait_read_frame()
+              log("Got frame...", response)
+              data =  response['rf_data']
+              payload = struct.unpack('ffffLb', data)
+              kind = payload[5]
+              dictionary = deserializers[kind](payload)
+              log(payload, dictionary)
+              timestamp = str(payload[4])
+              uploader = SampleUploader(sys.argv[1], None)
+              dictionary["gps_lat"] = uploader.config.get("databoat", "gps_lat")
+              dictionary["gps_long"] = uploader.config.get("databoat", "gps_long")
+              dictionary["SensorNode"] = uploader.config.get("databoat", "SensorNode")
+              sample = {
+                "t_local": timestamp,
+                "data": dictionary
+              }
+              samples = uploader.save(timestamp, None, sample)
+              uploader.upload(samples)
+            except KeyboardInterrupt:
+              sys.exit(0)
+            except Exception as i:
+              log(i)
+              time.sleep(1)
+        except Exception as i:
+          log(i)
+          time.sleep(1)
+        finally:
+          device.close()
+
+if __name__ == "__main__":
+  log("Starting...")
+  listener = XbeeListener("/var/run/xbee.pid")
+  if "start" in sys.argv:
+    listener.start()
+  if "restart" in sys.argv:
+    listener.restart()
+  if "stop" in sys.argv:
+    listener.stop()
+  else:
+    listener.run()
