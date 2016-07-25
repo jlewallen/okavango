71c8a0680d3671d3e0bac962773040e87e3657a5
Jacob Lewallen <jlewallen@gmail.com>
2016-07-25 19:02:50 -0400

    Cleanup, add putty.exe

--- XBeeDryRun/XBeeDryRun.ino
+++ XBeeDryRun/XBeeDryRun.ino
@@ -1,11 +1,10 @@
 #include <XBee.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 #define XBEE_DESTINATION_HIGH 0x0013A200
 #define XBEE_DESTINATION_LOW 0x40E677BF
 #include "XBeeUtilities.h"
-
-uint32_t counter = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -17,11 +16,8 @@ void setup() {
 
 void loop() {
   Serial.print("Sending ");
-  Serial.println(counter);
 
   memset((void *)&payload, 0, sizeof(payload));
-  longDelayAndAttemptToSendPacket(60L * 1000L * 30L);
+  longDelayAndAttemptToSendPacket(60L * 1000L);
   resetArduino();
-  
-  counter++;
 }
