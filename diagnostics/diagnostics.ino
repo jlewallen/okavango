d442347340a8454a6e4e92db3feb4d521a81c5f6
Jacob Lewallen <jlewallen@gmail.com>
2016-08-23 18:24:25 -0400

    Logging in diagnostics.

--- diagnostics/diagnostics.ino
+++ diagnostics/diagnostics.ino
@@ -1,3 +1,4 @@
+#include <SD.h>
 #include "Platforms.h"
 #include "core.h"
 #include "Repl.h"
@@ -43,23 +44,23 @@ public:
 
 private:
     void logHeader(rf95_header_t *header, fk_network_packet_t *packet, const char *kind) {
-        Serial.print(header->from, HEX);
-        Serial.print("->");
-        Serial.print(header->to, HEX);
-        Serial.print(" [");
-        Serial.print(header->id, HEX);
-        Serial.print(",");
-        Serial.print(header->flags, HEX);
-        Serial.print(",");
-        Serial.print(header->rssi);
-        Serial.print("] ");
-        Serial.print(kind);
-        Serial.print(" ");
-        Serial.print(packet->name);
+        logPrinter.print(header->from, HEX);
+        logPrinter.print("->");
+        logPrinter.print(header->to, HEX);
+        logPrinter.print(" [");
+        logPrinter.print(header->id, HEX);
+        logPrinter.print(",");
+        logPrinter.print(header->flags, HEX);
+        logPrinter.print(",");
+        logPrinter.print(header->rssi);
+        logPrinter.print("] ");
+        logPrinter.print(kind);
+        logPrinter.print(" ");
+        logPrinter.print(packet->name);
     }
 
     void log(rf95_header_t *header, fk_network_packet_t *packet, size_t packetSize) {
-        Serial.println();
+        DEBUG_PRINTLN();
 
         switch (packet->kind) {
         case FK_PACKET_KIND_PING: {
@@ -67,7 +68,7 @@ private:
             memcpy((uint8_t *)&ping, (uint8_t *)packet, sizeof(fk_network_ping_t));
 
             logHeader(header, &ping.fk, "PING");
-            Serial.println();
+            DEBUG_PRINTLN();
             break;
         }
         case FK_PACKET_KIND_PONG: {
@@ -75,9 +76,9 @@ private:
             memcpy((uint8_t *)&pong, (uint8_t *)packet, sizeof(fk_network_pong_t));
 
             logHeader(header, &pong.fk, "PONG");
-            Serial.print("time=");
-            Serial.print(pong.time);
-            Serial.println();
+            DEBUG_PRINT("time=");
+            DEBUG_PRINT(pong.time);
+            DEBUG_PRINTLN();
             break;
         }
         case FK_PACKET_KIND_ACK: {
@@ -85,7 +86,7 @@ private:
             memcpy((uint8_t *)&ack, (uint8_t *)packet, sizeof(fk_network_ack_t));
 
             logHeader(header, &ack.fk, "ACK");
-            Serial.println();
+            DEBUG_PRINTLN();
             break;
         }
         case FK_PACKET_KIND_ATLAS_SENSORS: {
@@ -93,15 +94,15 @@ private:
             memcpy((uint8_t *)&atlas_sensors, (uint8_t *)packet, sizeof(atlas_sensors_packet_t));
 
             logHeader(header, &atlas_sensors.fk, "ATLAS_SENSORS");
-            Serial.print("time=");
-            Serial.print(atlas_sensors.time);
-            Serial.print(" battery=");
-            Serial.print(atlas_sensors.battery);
+            DEBUG_PRINT("time=");
+            DEBUG_PRINT(atlas_sensors.time);
+            DEBUG_PRINT(" battery=");
+            DEBUG_PRINT(atlas_sensors.battery);
             for (uint8_t i = 0; i < FK_WEATHER_STATION_PACKET_NUMBER_VALUES; ++i) {
-                Serial.print(" ");
-                Serial.print(String(atlas_sensors.values[i], 2));
+                DEBUG_PRINT(" ");
+                DEBUG_PRINT(String(atlas_sensors.values[i], 2));
             }
-            Serial.println();
+            DEBUG_PRINTLN();
             break;
         }
         case FK_PACKET_KIND_WEATHER_STATION: {
@@ -109,10 +110,10 @@ private:
             memcpy((uint8_t *)&weather_station, (uint8_t *)packet, sizeof(weather_station_packet_t));
 
             logHeader(header, &weather_station.fk, "WEATHER_STATION");
-            Serial.print("time=");
-            Serial.print(weather_station.time);
-            Serial.print(" battery=");
-            Serial.println();
+            DEBUG_PRINT("time=");
+            DEBUG_PRINT(weather_station.time);
+            DEBUG_PRINT(" battery=");
+            DEBUG_PRINTLN();
             break;
         }
         case FK_PACKET_KIND_DATA_BOAT_SENSORS: {
@@ -120,35 +121,35 @@ private:
             memcpy((uint8_t *)&data_boat, (uint8_t *)packet, sizeof(data_boat_packet_t));
 
             logHeader(header, &data_boat.fk, "DATA_BOAT_SENSORS");
-            Serial.print(" ");
-            Serial.print(String(data_boat.latitude, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.longitude, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.altitude, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.speed, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.angle, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.water_temperature, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.pressure, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.humidity, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.temperature, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.conductivity, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.salinity, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.ph, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.dissolved_oxygen, 2));
-            Serial.print(" ");
-            Serial.print(String(data_boat.orp, 2));
-            Serial.println();
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.latitude, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.longitude, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.altitude, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.speed, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.angle, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.water_temperature, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.pressure, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.humidity, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.temperature, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.conductivity, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.salinity, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.ph, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.dissolved_oxygen, 2));
+            DEBUG_PRINT(" ");
+            DEBUG_PRINT(String(data_boat.orp, 2));
+            DEBUG_PRINTLN();
             break;
         }
         }
@@ -168,7 +169,7 @@ public:
     bool doWork() {
         return false;
     }
-    
+
     void handle(String command) {
         if (command == "tx") {
             fk_network_force_transmission_t  force_transmission;
@@ -197,11 +198,20 @@ void setup() {
     pinMode(PIN_SD_CS, OUTPUT);
     digitalWrite(PIN_SD_CS, HIGH);
 
+    if (!SD.begin(PIN_SD_CS)) {
+        DEBUG_PRINTLN(F("SD Missing"));
+        platformCatastrophe(PIN_RED_LED);
+    }
+
+    #ifdef FK_WRITE_LOG_FILE
+    logPrinter.open();
+    #endif
+
     Serial.println("Begin");
 
     // corePlatform.setup();
 
-    Serial.println("Loop");
+    DEBUG_PRINTLN("Loop");
 }
 
 void loop() {
@@ -223,4 +233,3 @@ void loop() {
 }
 
 // vim: set ft=cpp:
-
