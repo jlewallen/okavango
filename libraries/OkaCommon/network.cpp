219dd6bf2b0f1d6c2a16c15f4b55ccea63bb5879
Jacob Lewallen <jlewallen@gmail.com>
2016-11-11 20:44:42 -0500

    Force sonar packets to use our local time.

--- libraries/OkaCommon/network.cpp
+++ libraries/OkaCommon/network.cpp
@@ -137,7 +137,12 @@ void NetworkProtocolState::handle(fk_network_packet_t *packet, size_t packetSize
 
             DEBUG_PRINTLN("Queuing");
 
-            queue->enqueue((uint8_t *)packet);
+            {
+                sonar_station_packet_t sonar_station_packet;
+                memcpy((void *)&sonar_station_packet, packet, sizeof(sonar_station_packet_t));
+                sonar_station_packet.time = SystemClock->now();
+                queue->enqueue((uint8_t *)&sonar_station_packet, sizeof(sonar_station_packet_t));
+            }
 
             sendAck();
         }
