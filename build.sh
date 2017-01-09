ce0b40813b9f4d370754a207e3412d109e504671
Jacob Lewallen <jlewallen@gmail.com>
2017-01-09 11:06:12 -0500

    Use gradle for build.

--- build.sh
+++ build.sh
@@ -3,30 +3,14 @@
 set -e
 set -x
 
-BUILD=`pwd`/building/build.sh
-
-pushd tests
-for a in *; do
-    pushd $a
-    $BUILD -m -b
-    $BUILD -a -b
-    popd
-done
+pushd sensors
+gradle clean build
 popd
 
-pushd sensors
-$BUILD -m -b
+pushd ngdemo
+gradle clean build
 popd 
 
 pushd collector
-$BUILD -m -b
+gradle clean build
 popd
-
-pushd weather-shield
-$BUILD -a -b
-popd
-
-pushd fona-driver
-$BUILD -f -b
-popd
-
