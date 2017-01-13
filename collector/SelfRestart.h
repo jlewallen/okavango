e8fc54d018c7062e4298d7b1214c811a2887b310
Jacob Lewallen <jlewallen@gmail.com>
2017-01-13 18:30:45 -0500

    We don't need to know if we just had a mandatory restart anymore.

--- collector/SelfRestart.h
+++ collector/SelfRestart.h
@@ -2,7 +2,6 @@
 #define SELF_RESTART_H_INCLUDED
 
 #define MANDATORY_RESTART_INTERVAL   (1000 * 60 * 60 * 6)
-#define MANDATORY_RESTART_FILE       "RESUME.INF"
 
 #include <SD.h>
 
@@ -10,25 +9,9 @@ class SelfRestart {
 public:
     static bool isRestartNecessary() {
         if (millis() > MANDATORY_RESTART_INTERVAL) {
-            File file = SD.open(MANDATORY_RESTART_FILE, FILE_WRITE);
-            if (!file) {
-                // Consider not doing the restart now? Maybe waiting another interval?
-                DEBUG_PRINTLN("Error creating restart indicator file.");
-            }
-            else {
-                file.close();
-            }
             DEBUG_PRINTLN("Mandatory restart triggered.");
             logPrinter.flush();
             delay(1000);
-            return true;
-        }
-        return false;
-    }
-
-    static bool didWeJustRestart() {
-        if (SD.exists(MANDATORY_RESTART_FILE)) {
-            SD.remove(MANDATORY_RESTART_FILE);
             return true;
         }
         return false;
