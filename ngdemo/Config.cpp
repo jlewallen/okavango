663b05318d9db895a18fa1bf3a8e95e2405f3a9c
Jacob Lewallen <jlewallen@gmail.com>
2017-03-30 11:37:53 -0700

    Parse URL when reading config.

--- ngdemo/Config.cpp
+++ ngdemo/Config.cpp
@@ -7,12 +7,32 @@ bool Config::read() {
 
     File urlFile = SD.open("URL.CFG");
     if (urlFile) {
-        url = urlFile.readString();
+        String url = urlFile.readString();
         url.trim();
         urlFile.close();
 
         DEBUG_PRINT("URL: ");
         DEBUG_PRINTLN(url);
+
+        String schema = "http://";
+        int32_t schemaIndex = url.indexOf(schema);
+        if (schemaIndex < 0) {
+            return false;
+        }
+
+        int32_t pathSlashIndex = url.indexOf('/', schema.length());
+        if (pathSlashIndex < 0) {
+            return false;
+        }
+
+        urlServer = url.substring(schema.length(), pathSlashIndex);
+        urlPath = url.substring(pathSlashIndex);
+
+        DEBUG_PRINT("Server: ");
+        DEBUG_PRINTLN(urlServer);
+
+        DEBUG_PRINT("Path: ");
+        DEBUG_PRINTLN(urlPath);
     }
     else {
         DEBUG_PRINTLN("No url.cfg");
